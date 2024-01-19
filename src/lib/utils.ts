e6b682aea1c8921564cfae45cddfe9b0ea8e855f
ndonat <ndonat@ad.commsys.com>
2024-01-19 17:58:27 -0500

    default week-year dropdowns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,11 +2,22 @@ type DateStyle = Intl.DateTimeFormatOptions['dateStyle'];
 type TimeStyle = Intl.DateTimeFormatOptions['timeStyle'];
 type TimeZone = Intl.DateTimeFormatOptions['timeZone'];
 
+function daysIntoYear(date: Date): number {
+    return (
+        (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+            Date.UTC(date.getFullYear(), 0, 0)) /
+        24 /
+        60 /
+        60 /
+        1000
+    );
+}
+
 export function formatDate(date: string,
     dateStyle: DateStyle = undefined,
     timeStyle: TimeStyle = undefined,
     timeZone: TimeZone = undefined,
-    locales: string = 'en-US') {
+    locales: string = 'en-US'): string {
 
     const dateToFormat = new Date(date);
     const dateFormatter = new Intl.DateTimeFormat(locales, { dateStyle, timeStyle, timeZone });
@@ -20,7 +31,17 @@ export function formatDate(date: string,
     return dateFormatter.format(dateToFormat);
 }
 
-export function isEmpty(obj: object) {
+export function weekOfYearSince(
+    weekOfDate: Date,
+    sinceDate: Date = new Date(1999, 3, 1),
+    useSameYear: boolean = true
+): number {
+    if (useSameYear) sinceDate.setFullYear(weekOfDate.getFullYear());
+    let daysPassed = (weekOfDate.getTime() - sinceDate.getTime()) / 24 / 60 / 60 / 1000;
+    return 1 + Math.floor(daysPassed / 7);
+}
+
+export function isEmpty(obj: object): boolean {
     for (var x in obj) { return false; }
     return true;
 }
--- src/routes/api/(authed)/sitedates/[sitedateid]/+page.svelte
+++ src/routes/api/(authed)/sitedates/[sitedateid]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import DoubledContainer from '$lib/components/DoubledContainer.svelte';
-    import { formatDate } from '$lib/utils';
+    import { formatDate, weekOfYearSince } from '$lib/utils';
     import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
     import { SlideToggle } from '@skeletonlabs/skeleton';
     export let data;
@@ -20,68 +20,62 @@
         }
     }
 
-    function daysIntoYear(date: Date) {
-        return (
-            (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-                Date.UTC(date.getFullYear(), 0, 0)) /
-            24 /
-            60 /
-            60 /
-            1000
-        );
-    }
-
-    function weekSince(date: Date) {
-        let x = new Date(date.getFullYear(), 3, 1);
-        let daysPassed = (date.getTime() - x.getTime()) / 24 / 60 / 60 / 1000;
-        return 1 + Math.floor(daysPassed / 7);
-    }
-
     let startTemp: string;
     let endTemp: string;
     let useF = true;
+
+    let recordDate = new Date(data.siteDate.recordDate);
+    let recordYear = new Date(data.siteDate.recordDate).getFullYear();
+    let recordWeek = weekOfYearSince(new Date(data.siteDate.recordDate));
+
     $: startTemp = String(data.siteDate.startTemp);
     $: endTemp = String(data.siteDate.endTemp);
 
-    console.log(data.siteRecordDates);
+    //console.log(data.siteRecordDates);
     const yrs = Array.from(data.siteRecordDates).map((y) => new Date(y.recordDate).getFullYear());
     const years = [...new Set(yrs)];
     const weeks = Array.from(data.siteRecordDates).map((w) => ({
         siteDateId: w.siteDateId,
         year: new Date(w.recordDate).getFullYear(),
-        week: weekSince(new Date(w.recordDate)),
+        week: weekOfYearSince(new Date(w.recordDate)),
     }));
-    console.log(years);
-    console.log(weeks);
+    //console.log(years);
+    //console.log(weeks);
 
     //console.log(data.siteObservations[0]);
 </script>
 
 <DoubledContainer basisLeft="basis-2/5" basisRight="basis-3/5">
     <svelte:fragment slot="left">
-        <h2>{data.siteDate.site.siteName}</h2>
+        <h2 class="flex flex-row justify-between">
+            <div>{data.siteDate.site.siteName}</div>
+            <div>
+                Record Date: {formatDate(recordDate.toISOString(), 'medium', undefined)}
+            </div>
+        </h2>
         <hr />
         <div>
             <div class="flex flex-row justify-between">
                 <div>
                     year:{data.siteDate.year}&nbsp;&nbsp;week: {data.siteDate.week}
                 </div>
-                <div>
-                    Record Date: {formatDate(
-                        new Date(data.siteDate.recordDate).toISOString(),
-                        'medium',
-                        undefined
-                    )}
-                </div>
             </div>
-            <select>
+            <select class="select w-32">
                 {#each years as year}
-                    <option>{year}</option>
+                    {#if year === recordYear}
+                        <option selected>{year}</option>
+                    {:else}
+                        <option>{year}</option>
+                    {/if}
                 {/each}
             </select>
-            <select>
+            <select class="select w-32">
                 {#each weeks as week}
-                    <option value={week.siteDateId}>{week.year} {week.week}</option>
+                    {#if week.year === recordYear && week.week === recordWeek}
+                        <option value={week.siteDateId} selected>{week.year} {week.week}</option>
+                    {:else}
+                        <option value={week.siteDateId}>{week.year} {week.week}</option>
+                    {/if}
                 {/each}
             </select>
             <div>
@@ -91,11 +85,25 @@
                 <AccordionItem open>
                     <svelte:fragment slot="summary">Times</svelte:fragment>
                     <svelte:fragment slot="content">
-                        <div class="pl-4">
-                            startTime: {data.siteDate.startTime}
+                        <div class="pl-4 flex flex-row">
+                            <span class="basis-20 text-nowrap text-right">Start Time:</span>
+                            <span class="pl-2 w-24 text-right">
+                                {formatDate(
+                                    new Date(data.siteDate.startTime).toISOString(),
+                                    undefined,
+                                    'short'
+                                )}
+                            </span>
                         </div>
-                        <div class="pl-4">
-                            endTime: {data.siteDate.endTime}
+                        <div class="pl-4 flex flex-row">
+                            <span class="basis-20 text-nowrap text-right">End Time:</span>
+                            <span class="pl-2 w-24 text-right">
+                                {formatDate(
+                                    new Date(data.siteDate.endTime).toISOString(),
+                                    undefined,
+                                    'short'
+                                )}
+                            </span>
                         </div>
                     </svelte:fragment>
                 </AccordionItem>
@@ -117,19 +125,21 @@
                     </svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            startTemp: {startTemp}
+                            Start Temp: {data.siteDate.startTemp}
                         </div>
-                        <div class="pl-4">endTemp: {endTemp}</div></svelte:fragment
+                        <div class="pl-4">
+                            End Temp: {data.siteDate.endTemp}
+                        </div></svelte:fragment
                     >
                 </AccordionItem>
                 <AccordionItem>
                     <svelte:fragment slot="summary">Cloud cover (&percnt;)</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            startClouds: {data.siteDate.startClouds}
+                            Start Clouds: {data.siteDate.startClouds}
                         </div>
                         <div class="pl-4">
-                            endClouds: {data.siteDate.endClouds}
+                            End Clouds: {data.siteDate.endClouds}
                         </div></svelte:fragment
                     >
                 </AccordionItem>
@@ -137,16 +147,16 @@
                     <svelte:fragment slot="summary">Wind</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            startWindDir: {data.siteDate.startWindDir}
+                            Start Wind Dir: {data.siteDate.startWindDir}
                         </div>
                         <div class="pl-4">
-                            endWindDir: {data.siteDate.endWindDir}
+                            End Wind Dir: {data.siteDate.endWindDir}
                         </div>
                         <div class="pl-4">
-                            startWindMPH: {data.siteDate.startWindMPH}
+                            Start Wind MPH: {data.siteDate.startWindMPH}
                         </div>
                         <div class="pl-4">
-                            endWindMPH: {data.siteDate.endWindMPH}
+                            End Wind MPH: {data.siteDate.endWindMPH}
                         </div></svelte:fragment
                     >
                 </AccordionItem>
@@ -154,63 +164,63 @@
                     <svelte:fragment slot="summary">Weather</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            w1: {data.siteDate.w1}
+                            Weather 1: {data.siteDate.w1}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w2: {data.siteDate.w2}
+                            Weather 2: {data.siteDate.w2}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w3: {data.siteDate.w3}
+                            Weather 3: {data.siteDate.w3}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w4: {data.siteDate.w4}
+                            Weather 4: {data.siteDate.w4}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w5: {data.siteDate.w5}
+                            Weather 5: {data.siteDate.w5}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w6: {data.siteDate.w6}
+                            Weather 6: {data.siteDate.w6}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w7: {data.siteDate.w7}
+                            Weather 7: {data.siteDate.w7}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w8: {data.siteDate.w8}
+                            Weather 8: {data.siteDate.w8}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w9: {data.siteDate.w9}
+                            Weather 9: {data.siteDate.w9}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w10: {data.siteDate.w10}
+                            Weather 10: {data.siteDate.w10}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w11: {data.siteDate.w11}
+                            Weather 11: {data.siteDate.w11}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w12: {data.siteDate.w12}
+                            Weather 12: {data.siteDate.w12}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w13: {data.siteDate.w13}
+                            Weather 13: {data.siteDate.w13}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w14: {data.siteDate.w14}
+                            Weather 14: {data.siteDate.w14}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                         <div class="pl-4">
-                            w15: {data.siteDate.w15}
+                            Weather 15: {data.siteDate.w15}
                             <span class="bg-slate-800">Weather @default(U)</span>
                         </div>
                     </svelte:fragment>
@@ -269,16 +279,16 @@
                     <svelte:fragment slot="summary">Larva</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            larvaObA: {data.siteDate.larvaObA}
+                            Larva Ob A: {data.siteDate.larvaObA ?? ''}
                         </div>
                         <div class="pl-4">
-                            larvaObB: {data.siteDate.larvaObB}
+                            Larva Ob B: {data.siteDate.larvaObB ?? ''}
                         </div>
                         <div class="pl-4">
-                            larvaObC: {data.siteDate.larvaObC}
+                            Larva Ob C: {data.siteDate.larvaObC ?? ''}
                         </div>
                         <div class="pl-4">
-                            larvaObD: {data.siteDate.larvaObD}
+                            Larva Ob D: {data.siteDate.larvaObD ?? ''}
                         </div></svelte:fragment
                     >
                 </AccordionItem>
@@ -286,45 +296,49 @@
                     <svelte:fragment slot="summary">Energy/blooming</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            energySource1: {data.siteDate.energySource1}
+                            Energy Source 1: {data.siteDate.energySource1 ?? ''}
                         </div>
                         <div class="pl-4">
-                            energySource2: {data.siteDate.energySource2}
+                            Energy Source 2: {data.siteDate.energySource2 ?? ''}
                         </div>
                         <div class="pl-4">
-                            energySource3: {data.siteDate.energySource3}
+                            Energy Source 3: {data.siteDate.energySource3 ?? ''}
                         </div>
                         <div class="pl-4">
-                            energySource4: {data.siteDate.energySource4}
+                            Energy Source 4: {data.siteDate.energySource4 ?? ''}
                         </div>
                         <div class="pl-4">
-                            flowersInBloom: {data.siteDate.flowersInBloom}
+                            Flowers in Bloom: {data.siteDate.flowersInBloom ?? ''}
                         </div></svelte:fragment
                     >
                 </AccordionItem>
                 <div class="pl-4">
-                    fieldNotes: {data.siteDate.fieldNotes}
+                    Field Notes: {data.siteDate.fieldNotes ?? ''}
                 </div>
                 <AccordionItem>
-                    <svelte:fragment slot="summary">Data history</svelte:fragment>
+                    <svelte:fragment slot="summary">Change history</svelte:fragment>
                     <svelte:fragment slot="content">
                         <div class="pl-4">
-                            createdBy: {data.siteDate.createdBy}
+                            Created By: {data.siteDate.createdBy ?? ''}
                         </div>
                         <div class="pl-4">
-                            createdAt: {data.siteDate.createdAt}
+                            Created At: {formatDate(
+                                new Date(data.siteDate.createdAt).toISOString(),
+                                'medium',
+                                'medium'
+                            )}
                         </div>
                         <div class="pl-4">
-                            updatedBy: {data.siteDate.updatedBy}
+                            Updated By: {data.siteDate.updatedBy ?? ''}
                         </div>
                         <div class="pl-4">
-                            updatedAt: {data.siteDate.updatedAt}
+                            Updated At: {data.siteDate.updatedAt ?? ''}
                         </div>
                         <div class="pl-4">
-                            confirmBy: {data.siteDate.confirmBy}
+                            Confirm By: {data.siteDate.confirmBy ?? ''}
                         </div>
                         <div class="pl-4">
-                            confirmAt: {data.siteDate.confirmAt}
+                            Confirm At: {data.siteDate.confirmAt ?? ''}
                         </div></svelte:fragment
                     >
                 </AccordionItem>
@@ -333,7 +347,15 @@
     </svelte:fragment>
 
     <svelte:fragment slot="right">
-        <div>
+        <h2 class="flex flex-row justify-between">
+            <div>{data.siteDate.site.siteName}</div>
+            <div>
+                year: {recordYear}
+                Week: {recordWeek}
+            </div>
+        </h2>
+        <hr />
+        <div class="mt-2">
             {#each data.siteObservations as siteObservation}
                 <div class="card flex">
                     <div class="flex">
