e80a882ab8f0ad3df15cd792efa502d9aedb50b5
ndonat <ndonat@ad.commsys.com>
2024-01-31 11:11:05 -0500

    Initial addition of organization and app configs to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -26,6 +26,73 @@ await updateRoles();
 /*
 */
 
+let templateOganizationId: number;
+let mainOrganizationId: number;
+
+async function updateOrganizations() {
+  console.log('Creating Organizations');
+
+  let organization = await db.organization.create({
+    data: {
+      name: 'TEMPLATE'
+    }
+  });
+
+  templateOganizationId = organization.id;
+
+  organization = await db.organization.create({
+    data: {
+      name: 'Ohio Lepidopterists'
+    }
+  });
+
+  mainOrganizationId = organization.id;
+}
+
+async function updateAppConfigs() {
+  console.log('Creating App Configs');
+
+  await db.appConfig.createMany({
+    data: [
+      { configName: 'modeDebug', configType: 'boolean', configValue: 'true', organizationId: templateOrganizationId },
+      { configName: 'owner', configType: 'string', configValue: 'Template Owner', organizationId: templateOrganizationId },
+      { configName: 'title', configType: 'string', configValue: 'Template Title', organizationId: templateOrganizationId },
+      { configName: 'description', configType: 'string', configValue: 'Template Description', organizationId: templateOrganizationId },
+      { configName: 'url', configType: 'string', configValue: 'http://localhost:5173/', organizationId: templateOrganizationId },
+      { configName: 'showAnimatedIcon', configType: 'boolean', configValue: 'false', organizationId: templateOrganizationId },
+      { configName: 'showAppBar', configType: 'boolean', configValue: 'true', organizationId: templateOrganizationId },
+      { configName: 'showFooter', configType: 'boolean', configValue: 'false', organizationId: templateOrganizationId },
+      { configName: 'showAvatar', configType: 'boolean', configValue: 'false', organizationId: templateOrganizationId },
+      { configName: 'showStateProvince', configType: 'boolean', configValue: 'false', organizationId: templateOrganizationId },
+      { configName: 'monitorStartYear', configType: 'number', configValue: 1995, organizationId: templateOrganizationId },
+      { configName: 'monitorSeason', configType: 'string', configValue: '{"start":{"year":1995,"month":4,"day":1},"end":{"year":9999,"month":10,"day":1}}', organizationId: templateOrganizationId },
+      { configName: 'initialDateRangeChoice', configType: 'boolean', configValue: 'true', organizationId: templateOrganizationId },
+      { configName: 'initialHideUnmonitedChoice', configType: 'boolean', configValue: 'true', organizationId: templateOrganizationId },
+      { configName: 'initialUseLatinChoice', configType: 'boolean', configValue: 'true', organizationId: templateOrganizationId },
+    ]
+  });
+
+  await db.appConfig.createMany({
+    data: [
+      { configName: 'modeDebug', configType: 'boolean', configValue: 'true', organizationId: mainOrganizationId },
+      { configName: 'owner', configType: 'string', configValue: 'Ohio Lepidopterists &copy;1995', organizationId: mainOrganizationId },
+      { configName: 'title', configType: 'string', configValue: 'Learn Svelte(kit), Vite, TailwindCSS, SkeletonUI and Prisma', organizationId: mainOrganizationId },
+      { configName: 'description', configType: 'string', configValue: "Ohio Lepidopterists' Long-Term Monitering of Ohio Butterflies", organizationId: mainOrganizationId },
+      { configName: 'url', configType: 'string', configValue: 'http://localhost:5173/', organizationId: mainOrganizationId },
+      { configName: 'showAnimatedIcon', configType: 'boolean', configValue: 'false', organizationId: mainOrganizationId },
+      { configName: 'showAppBar', configType: 'boolean', configValue: 'true', organizationId: mainOrganizationId },
+      { configName: 'showFooter', configType: 'boolean', configValue: 'false', organizationId: mainOrganizationId },
+      { configName: 'showAvatar', configType: 'boolean', configValue: 'false', organizationId: mainOrganizationId },
+      { configName: 'showStateProvince', configType: 'boolean', configValue: 'false', organizationId: mainOrganizationId },
+      { configName: 'monitorStartYear', configType: 'number', configValue: 1995, organizationId: mainOrganizationId },
+      { configName: 'monitorSeason', configType: 'string', configValue: '{"start":{"year":1995,"month":4,"day":1},"end":{"year":9999,"month":10,"day":1}}', organizationId: mainOrganizationId },
+      { configName: 'initialDateRangeChoice', configType: 'boolean', configValue: 'true', organizationId: mainOrganizationId },
+      { configName: 'initialHideUnmonitedChoice', configType: 'boolean', configValue: 'true', organizationId: mainOrganizationId },
+      { configName: 'initialUseLatinChoice', configType: 'boolean', configValue: 'true', organizationId: mainOrganizationId },
+    ]
+  });
+}
+
 // Consider making role into ENUM
 async function updateRoles() {
   console.log('Creating Roles and Users');
@@ -39,6 +106,7 @@ async function updateRoles() {
           username: "stella",
           passwordHash: "$2b$10$bjXz/irXJrUh8gvG8fidZepQN0BrN2/d2R2RshizCHYSI.FP74s8G",
           userAuthToken: "dc1c44da-fa33-4fb7-82cc-b5baa7522c4d",
+          organizationId: mainOrganizationId
         }
       }
     }
@@ -53,6 +121,7 @@ async function updateRoles() {
           username: "snell",
           passwordHash: "$2b$10$iAIf7B4I9aUy9ZklbY.yy.GuJ0U3HjpttqEMiHb5zwvoMsYOqDXFy",
           userAuthToken: "70e2e703-06d9-4ebc-bc39-889d85f9db4d",
+          organizationId: mainOrganizationId
         }]
       }
     },
@@ -67,6 +136,7 @@ async function updateRoles() {
           username: "nate",
           passwordHash: "$2b$10$09GTBhmT0rC41GOoOvgw6.oGTbMEKZaxQ3spCEwRnmXjc6jxusoEC",
           userAuthToken: "10b4f4bc-8a3d-4df4-8112-98eea730fc24",
+          organizationId: mainOrganizationId
         }]
       }
     },
